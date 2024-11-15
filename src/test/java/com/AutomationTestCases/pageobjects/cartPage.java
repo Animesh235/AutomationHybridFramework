6e79ba9b53ccc8cd16bed1e84277e6276bc07b27
Animesh Panda <157322561+Animesh235@users.noreply.github.com>
2024-11-16 00:14:16 +0530

    Add files via upload

--- src/test/java/com/AutomationTestCases/pageobjects/cartPage.java
+++ src/test/java/com/AutomationTestCases/pageobjects/cartPage.java
@@ -1,12 +1,46 @@
 package com.AutomationTestCases.pageobjects;
 
-import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
-public class cartPage {
- WebDriver pagedriver;
-	
-	public cartPage(WebDriver remotedriver) {
-		PageFactory.initElements(remotedriver, this.pagedriver);
- }
+import com.AutomationTestCases.testscripts.BaseClass;
+
+//version1
+
+//public class cartPage {
+// WebDriver pagedriver;
+//	
+//	public cartPage(WebDriver remotedriver) {
+//		PageFactory.initElements(remotedriver, this.pagedriver);
+// }
+//}
+
+//also we can wright on this way
+
+//version2
+
+public class cartPage extends BaseClass {
+	 
+		public cartPage() {
+			PageFactory.initElements(driver, this);
+	 }
 }
+
+//Explanation
+//Class Declaration: public class cartPage extends BaseClass
+//
+//cartPage is a class that represents the shopping cart page in the application.
+//It extends a base class (BaseClass), which likely contains common functionality, including the driver instance, that is shared across different page classes.
+//Constructor: public cartPage()
+//
+//This is a constructor for the cartPage class, which is called when you create an instance of cartPage.
+//Inside the constructor, there’s a call to PageFactory.initElements(driver, this);, which initializes the WebElements on this page.
+//PageFactory.initElements(driver, this);
+//
+//PageFactory is a Selenium class used to initialize WebElements with the @FindBy annotation, allowing for a more readable and organized approach to locating elements.
+//PageFactory.initElements(driver, this); does the following:
+//It takes the driver instance (probably from BaseClass) and associates it with the WebElements declared in this cartPage class.
+//this refers to the current instance of cartPage, so all @FindBy-annotated fields in this class will be initialized with the given driver.
+
+//For small projects or single-browser testing: Version 1 with a base class might be sufficient and easier to maintain.
+//For larger projects or parallel testing with multiple configurations: Version 2 would be preferable for greater control and flexibility.
--- src/test/java/com/AutomationTestCases/testscripts/BaseClass.java
+++ src/test/java/com/AutomationTestCases/testscripts/BaseClass.java
@@ -1,6 +1,15 @@
 package com.AutomationTestCases.testscripts;
 
+import org.testng.annotations.BeforeClass;
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 //import org.apache.logging.log4j.Logger;
 //import org.apache.logging.log4j.LogManager;
@@ -9,13 +18,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-
 import com.AutomationTestCases.utilities.ReadConfig;
 
 public class BaseClass {
 	
 	public static WebDriver driver;
+	public static Logger logger;
 	
 	//private static final Logger logger = LogManager.getLogger(BaseClass.class);
 	
@@ -39,23 +47,47 @@ public class BaseClass {
 	            throw new RuntimeException("Browser not supported: " + browsername);
 	        }
 	        
+	       //for logging
+			logger = LogManager.getLogger("Test Script");
+	        
 	        // Navigating to the specified URL and maximizing the window
 	        driver.get(url);
-	        //logger.info("Navigated to URL: " + url);
+	        
+	        logger.info("Navigated to URL: " + url);
+	        
 	        driver.manage().window().maximize();
-	        //logger.info("Browser window maximized.");
+	        
+	        logger.info("Browser window maximized.");
 	        
 	        // Set timeouts for better synchronization
 	        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
 	        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	        //logger.info("Page load timeout set to 5 seconds, implicit wait set to 10 seconds.");
+	        
+	        logger.info("Page load timeout set to 5 seconds, implicit wait set to 10 seconds.");
 	    }
 
+	
 	@AfterClass
     public void tearDown() {
         if (driver != null) {
             driver.quit();
-           // logger.info("Browser closed and resources released.");
+            logger.info("Browser closed and resources released.");
         }
     }
+	
+	   //user method to capture screen shot
+		public void captureScreenShot(WebDriver driver,String testName) throws IOException
+		{
+			//step1: convert webdriver object to TakesScreenshot interface
+			TakesScreenshot screenshot = ((TakesScreenshot)driver);
+			
+			//step2: call getScreenshotAs method to create image file
+			
+			File src = screenshot.getScreenshotAs(OutputType.FILE);
+			
+			File dest = new File(System.getProperty("user.dir") + "//Screenshots//" + testName + ".png");
+		
+			//step3: copy image file to destination
+			FileUtils.copyFile(src, dest);
+		}
 }
